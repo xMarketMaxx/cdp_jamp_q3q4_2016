fc0b84b8927df2a159be6a51b54475ca689bec2c
Maksym Pinchuk <Maksym_Pinchuk@epam.com>
2016-10-17 20:37:12 +0300

    add Encoding filter and update web.xml file

---
+++ M1/Hw1/simpleshop/src/main/java/com/epam/simpleshop/entry/filter/EncodingFilter.java
@@ -0,0 +1,38 @@
+package com.epam.simpleshop.entry.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+public class EncodingFilter implements Filter
+{
+
+    private String encoding = "utf-8";
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+        throws IOException, ServletException
+    {
+        request.setCharacterEncoding(encoding);
+        filterChain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+        String encodingParam = filterConfig.getInitParameter("encoding");
+        if (encodingParam != null)
+        {
+            encoding = encodingParam;
+        }
+    }
+
+    public void destroy()
+    {
+    }
+}
